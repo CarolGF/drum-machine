1553214a6276ce31d70784e65273faf9f97a91aa
CarolGF <karola_gf@hotmail.com>
2020-04-22 16:06:53 +0100

    Updates

---
+++ precache-manifest.43d735d26d3e4569b55d2aad0180e621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3750b1f71999b0186c49a24227bcf1cd",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "5e9141d092ba0c579c1a",
+    "url": "/drum-machine/static/css/main.2f3f291a.chunk.css"
+  },
+  {
+    "revision": "2f9c95a3e9d5ca0a01ca",
+    "url": "/drum-machine/static/js/2.84ad992f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.84ad992f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9141d092ba0c579c1a",
+    "url": "/drum-machine/static/js/main.3ee475ad.chunk.js"
+  },
+  {
+    "revision": "ac7d9540cfdb54ee17c9",
+    "url": "/drum-machine/static/js/runtime-main.3d96963e.js"
+  }
+]);
